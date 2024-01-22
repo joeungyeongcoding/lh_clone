681ae30e614b569eca10025d1a45e838a8be7779
joeungyeongcoding <joeungyeong.coding@gmail.com>
2024-01-22 13:15:47 +0900

    24-01-22 commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 $(function(){
-    //gnb all 메뉴 열기
-    $('.btn-list').click(function(){
-      $('#gnb').show();
-    });
-    $('.closebox').click(function(){
-      $('#gnb').hide();
-    });
-  
-    //main-nav
-    $('.main-nav').hover(function(){
-       $('.header-background').stop().slideToggle(200);
-       $('.main-nav a, .logo, .btn-h-line, .ri-global-line, .ri-search-line').stop().toggleClass("black-font");
-       $('.searchform,.btn-h-line').stop().toggleClass("black-border");
-    });
-  }); //jquery
+  //gnb all 메뉴 열기
+  $('.btn-list').click(function(){
+    $('#gnb').show();
+  });
+  $('.closebox').click(function(){
+    $('#gnb').hide();
+  });
+
+  //main-nav
+  $('.main-nav').hover(function(){
+     $('.header-background').stop().slideToggle(200);
+     $('.main-nav a, .logo, .btn-h-line, .ri-global-line, .ri-search-line').stop().toggleClass("black-font");
+     $('.searchform,.btn-h-line').stop().toggleClass("black-border");
+  });
+ 
+  $('.main-nav>li').hover(function(){
+     $(this).find('.d-submenu-main').toggle();
+  });
+
+}); //jquery
