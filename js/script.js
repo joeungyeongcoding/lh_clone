7de0b2bfb2561c6da96ea35fda8bac5e1d52c22c
joeungyeongcoding <joeungyeong.coding@gmail.com>
2024-01-25 13:12:01 +0900

    2024-01-25 commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function(){
   //gnb all 메뉴 열기
+  let count = 1;
+
   $('.btn-list').click(function(){
     $('#gnb').show();
   });
@@ -36,4 +38,27 @@ $(function(){
      $(this).find('span').addClass("act");
   });
 
+  //배너 애니메이션
+
+  // $('.banner-in').css('width', (504*4)+"px");
+  setInterval(mySlide, 5000);
+
+function mySlide(){
+count++;
+if(count > 4){
+  count=1;
+}
+$('.count').text(count);
+  $('.banner-in').animate({
+    'left':'-504px'
+  },500, function(){
+    $('.banner-in img:eq(0)').clone().appendTo('.banner-in');
+    $('.banner-in img:eq(0)').remove();
+    $('.banner-in').css('left',0);
+  });
+}
+
 }); //jquery
+
+
+
